cc795c934173bd3694870fbe308b4fade7b41a4d
Soros Wen <wenmingjun0725@gmail.com>
2021-09-10 11:05:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+import os 
+
+app = Flask(__name__)
+
+@app.route('/')
+def index(): 
+    workingdir = os.path.abspath(os.getcwd())
+    filepath = workingdir
+    return send_from_directory(filepath, 'Resume.pdf')
